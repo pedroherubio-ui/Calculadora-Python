12b42ad97ac3962d75ab8cfb5092ed6f1a5b4155
Pedro Henrique <pedroherubio@gmail.com>
2025-10-02 22:35:06 -0300

    Adicionando Loop While e segurando saida automatica

--- calculator.py
+++ calculator.py
@@ -1,34 +1,47 @@
-print("Menu Calculadora")
-print("Escolha o operador aritimético")
-try:
-    type_operator = int(input(f" Digite 1 para Soma \n Digite 2 para Subtração \n Digite 3 para Multiplicação \n Digite 4 para Divisão \n "))
-    if type_operator not in [1, 2 ,3 ,4]:
-        print("Numero invalido")
-    else:
-        try: 
-            num1 = int(input("Digite o primeiro numero: "))
-            num2 = int(input("Digite o segundo numero: "))
+continuar = "s"
+while True:
+    print("Menu Calculadora")
+    print("Escolha o operador aritimético")
+    try:
+        type_operator = int(input(f" Digite 1 para Soma \n Digite 2 para Subtração \n Digite 3 para Multiplicação \n Digite 4 para Divisão \n "))
+        if type_operator not in [1, 2 ,3 ,4]:
+            print("Numero invalido")
+        else:
+            try: 
+                num1 = int(input("Digite o primeiro numero: "))
+                num2 = int(input("Digite o segundo numero: "))
 
-            if type_operator == 1:
-                print("Menu Soma")
-                resultado = num1 + num2
-                print(f"O resultado da soma de {num1} + {num2} é: {resultado}")
-            elif type_operator == 2:
-                print("Menu Subtração")
-                resultado = num1 - num2
-                print(f"O resultado da subtração de {num1} - {num2} é: {resultado}")
-            elif type_operator == 3:
-                print("Menu Multiplicação")
-                resultado = num1 * num2
-                print(f"O resultado da multiplicação de {num1} multiplicado por {num2} é: {resultado}")
-            elif type_operator == 4:
-                try:    
-                    print("Menu Divisão")
-                    resultado = num1 / num2
-                    print(f"O resultado da divisão de {num1} divido por {num2} é: {resultado}")
-                except ZeroDivisionError:
-                    print("Não é possivel dividir o numero zero")
-        except ValueError: 
-                print("Digite apenas numeros válidos")
-except ValueError:
-    print("Erro: Digite apenas números válidos para escolher a operação.")
+                if type_operator == 1:
+                    print("Menu Soma")
+                    resultado = num1 + num2
+                    print(f"O resultado da soma de {num1} + {num2} é: {resultado}")
+                elif type_operator == 2:
+                    print("Menu Subtração")
+                    resultado = num1 - num2
+                    print(f"O resultado da subtração de {num1} - {num2} é: {resultado}")
+                elif type_operator == 3:
+                    print("Menu Multiplicação")
+                    resultado = num1 * num2
+                    print(f"O resultado da multiplicação de {num1} multiplicado por {num2} é: {resultado}")
+                elif type_operator == 4:
+                    try:    
+                        print("Menu Divisão")
+                        resultado = num1 / num2
+                        print(f"O resultado da divisão de {num1} divido por {num2} é: {resultado}")
+                    except ZeroDivisionError:
+                        print("Não é possivel dividir o numero zero ")
+            except ValueError: 
+                    print("Digite apenas numeros válidos")
+    except ValueError:
+        print("Erro: Digite apenas números válidos para escolher a operação.")
+
+    while True:
+        continuar = input("Deseja usar novamente a calculadora? (s/n) ").lower()
+        if continuar == "s":
+            break  # ou continue, dependendo da lógica
+        elif continuar == "n":
+            input("Pressione Enter para sair...")
+            exit()
+        else:
+            print("Digite uma opção válida (s ou n)")
+
